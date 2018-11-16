521436705dacaf3e01b22b96623bc15c23837d00
edfeld <edfeld1@gmail.com>
2018-11-16 14:09:12 -0800

    working on creating the table

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,30 +3,41 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burgers.js) to use its database functions.
-var burgers = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burgers.selectAll(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log("burgers controller.js hbsObject: ", hbsObject);
-      res.render("index", hbsObject);
+    console.log("db.burgers: ", db.burgers);
+    db.burgers.findAll({}).then(function(results) {
+      console.log("results: ", results);
+      res.render("index", results);
     });
+    // db.Burgers.findAll(function(data) {
+    //   var hbsObject = {
+    //     burgers: data
+    //   };
+    //   console.log("burgers controller.js hbsObject: ", hbsObject);
+    //   res.render("index", hbsObject);
+    // });
   });
   
   // Post is insert a new burger
   router.post("/api/burgers", function(req, res) {
     console.log("req.body: ", req.body);
-    burgers.insertOne([
-      "burger_name", "devoured"
-    ], [
-      req.body.burger_name, req.body.devoured
-    ], function(result) {
-      // Send back the ID of the new quote
+    db.burgers.create({
+      burger_name: req.body.burger_name, 
+      devoured: req.body.devoured
+    }).then(function(results){
       res.json({ id: result.insertId });
     });
+    // db.burgers.insertOne([
+    //   "burger_name", "devoured"
+    // ], [
+    //   req.body.burger_name, req.body.devoured
+    // ], function(result) {
+    //   // Send back the ID of the new quote
+    //   res.json({ id: result.insertId });
+    // });
   });
   
   // Put is an update 
@@ -35,10 +46,13 @@ router.get("/", function(req, res) {
   
     console.log("controller Put condition: ", condition);
     console.log("req.body.devoured: ", req.body);
-  
-    burgers.updateOne({
-      devoured: req.body.devoured
-    }, condition, function(result) {
+    db.burgers.update({
+      devoured: req.body.devoured}, {
+        where: {
+          id: req.body.id
+      }
+    }).then(function(results){
+      res.json(result);
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
@@ -46,6 +60,16 @@ router.get("/", function(req, res) {
         res.status(200).end();
       }
     });
+    // db.burgers.updateOne({
+    //   devoured: req.body.devoured
+    // }, condition, function(result) {
+    //   if (result.changedRows == 0) {
+    //     // If no rows were changed, then the ID must not exist, so 404
+    //     return res.status(404).end();
+    //   } else {
+    //     res.status(200).end();
+    //   }
+    // });
   });
 
 // Export routes for server.js to use.
--- models/burger.js
+++ models/burger.js
@@ -1,32 +1,15 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+/* Create a burger model with columns for "burger_name" (DataTypes.STRING), and "devoured" (DataTypes.BOOLEAN).*/
 
-var burgers = {
-  // call select all in the ORM
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
+module.exports = function(sequelize, DataTypes) {
+    var burgers = sequelize.define("burgers", {
+        burger_name: {
+            type: DataTypes.STRING 
+        },
+        devoured: { 
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
     });
-  },
-  // call the insertOne function in the ORM
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  // call the updateOne function in the ORM
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-//   delete: function(condition, cb) {
-//     orm.delete("burgers", condition, function(res) {
-//       cb(res);
-//     });
-//   }
+
+    return burgers;
 };
-
-// Export the database functions for the controller (burgersController.js).
-module.exports = burgers;
